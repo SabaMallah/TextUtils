d97375d41d410fe540046072d3d862dd22eafd5f
Saba Mallah <mallahsaba28@gmail.com>
2025-04-12 12:28:18 +0500

    using state

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -1,17 +1,20 @@
 import React from "react";
+import { useState } from "react";
 
-export default function TextForm() {
+export default function TextForm(props) {
+  const [text, setText] = useState("Enter Text Here");
   return (
     <>
+      <h2>{props.title}</h2>
       <div class="mb-3">
-        <label for="exampleFormControlTextarea1" class="form-label">
-          Example textarea
-        </label>
         <textarea
-          class="form-control"
+          className="form-control"
           id="exampleFormControlTextarea1"
           rows="3"
-        ></textarea>
+        >
+          {text}
+        </textarea>
+        <button className="btn btn-primary">Convert to Upper Case</button>
       </div>
     </>
   );
